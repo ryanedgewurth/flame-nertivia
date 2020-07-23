ed93e5fada2d2fbb74444302c4987ee220f55032
Edgewurth <32139009+Shorthorn-Studios@users.noreply.github.com>
2020-07-23 06:57:18 +0100

    Add files via upload

--- flamebot.js
+++ flamebot.js
@@ -1,12 +1,23 @@
+/* FLAME NERTIVIA BOT
+Programmed by: Edgewurth
+Current Version: 2020-07-15 Patch 
+Current Version Number: 0.2.0 */
+var vernum = "0.2.0"; // Update Every Build
+var vername = "2020-07-15 Patch"; // Update Every Build
 const NertiviaSys = require("nertivia.js");
 const bot = new NertiviaSys.Client();
 const InfoCmds = require("./cmd/info.js");
 const FunCmds = require("./cmd/fun.js");
 const EcoCmds = require("./cmd/eco.js");
+const SettingCmds = require("./cmd/settings.js");
 var fs = require('file-system');
 var levels 
+var userconf
 fs.readFile('./configs/levels.json', 'utf8', function(err, contents) {
     levels = JSON.parse(contents);
+});
+fs.readFile('./configs/userconf.json', 'utf8', function(err, contents) {
+    userconf = JSON.parse(contents);
 });
 var botToken = "NjY3MTMyNTU5NDc4Mjc5NzgyNA.b7DVHqhdHHu2J_DmW__fiU8TaScX3Oci9Wp3OQ3UWvc"; // This is your bot token, use this to login
 var botPrefix = "&"; // Change this to change the prefix used.
@@ -14,6 +25,37 @@ var contents = null
 bot.on("ready", () => {
     console.log(`Logged in as ${bot.user.tag}!`); // Indicates you were logged in.
 	console.log(`Flame Nertivia Bot By Edgewurth`);
+})
+var channelid = null
+bot.on("messageButtonClicked", (button, done) => {
+    if (button.id === "test") {
+        bot.channels.cache.get(channelid).send("Button '0w0' Test Successful", {
+            buttons: [{ id: 'unban', name: "undo" }]
+        })
+        done();
+	}
+	if (button.id === "userconfig") {
+        bot.channels.cache.get(channelid).send("Please Select a Setting Sub-Group", {
+            buttons: [{ id: 'userconfig_msgs', name: "Messages" },{ id: 'userconfig_privacy', name: "Privacy" },{ id: 'userconfig_etc', name: "Other" }]
+        })
+        done();
+	}
+	if (button.id === "userconfig_msgs") {
+        bot.channels.cache.get(channelid).send("Please Select a Setting to Modify", {
+            buttons: [{ id: 'userconfig_msgs_dmhelp', name: "Send Help to Inbox" }]
+        })
+        done();
+	}
+	if (button.id === "userconfig_msgs_dmhelp") {
+		if (typeof userconf[button.clickedByID + "_dmhelp"] === "undefined") {
+		userconf[button.clickedByID + "_dmhelp"] = "true";
+		} else if (userconf[button.clickedByID + "_dmhelp"] === "true") {
+			userconf[button.clickedByID + "_dmhelp"] = "false";
+		} else {
+			userconf[button.clickedByID + "_dmhelp"] = "true";
+		};
+        done("Set Send Help to Inbox to `" + userconf[msg.author.id + "_dmhelp"] + "` for <@${button.clickedByID}>");
+	}
 })
 
 
@@ -26,9 +68,12 @@ bot.on("message", msg => {
         msg.send(InfoCmds.about())
     }
 	if (cmd[0] === botPrefix + "settings") {
-        msg.send("Coming Soon")
+		/* This command section is fairly large */
+		channelid = msg.channelID
+		msg.send("Please select a settings group.", {buttons: [{id: 'userconfig', name: 'User'},{id: 'serverconfig', name: 'Server'}]} );
     }
 	if (cmd[0] === botPrefix + "help") {
+		msg.send("Please check your inbox to see the help list. If your private messages are closed, then please type ``&settings``, select User/Messages/Send Help to Inbox and press False.");
         msg.author.send(InfoCmds.help(cmd[1]))
     }
 	if (cmd[0] === botPrefix + "8ball") {
@@ -44,7 +89,7 @@ bot.on("message", msg => {
 		msg.send("Pong! The latency is " + latency.toFixed(0) + "ms");
     }
 	if (cmd[0] === botPrefix + "btntest") {
-        msg.send("I has button 0w0.",{buttons: [{id: 'test', name: '0w0'}]});
+        msg.send("I has button 0w0.",{buttons: [{id: 'test', name: '0w0'},{id: 'test2', name: '0w0 2'}]});
     }
 	if (cmd[0] === botPrefix + "bal") {
 		if (typeof levels[msg.author.id] === "undefined") {
---
+++ settings.js
@@ -0,0 +1,10 @@
+// Commands for "Settings" Section
+var fs = require('file-system');
+var settings 
+/* fs.readFile('../configs/settings.json', 'utf8', function(err, contents) {
+    settings = JSON.parse(contents);
+});
+exports.getSetting = function(settingid, user) {
+	var settingname = user + "_" + settingid;
+	return settings[settingname];
+} */
